a51c18f41eeac9735d2c93302ffe16a9c28fa30d
Alessandro Romano <alessandro.romano@lastminute.com>
2017-12-05 11:26:26 +0100

    added post and get for datapoints usign time-series

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const fastify = require('fastify')()
+const createTimeSeries = require('@axel92/time-series')
 
-fastify.get('/datapoint', (request, reply) => {
-  reply.send({ hello: 'world' })
+fastify.register(require('fastify-mongodb'), {
+  url: 'mongodb://localhost:27017/timeseries'
+})
+
+fastify.post('/datapoints', async (request, reply) => {
+  const { db } = fastify.mongo
+  const timeSeries = createTimeSeries(db)
+  const payload = request.body
+  const code = await timeSeries.addDataPoint(payload.data, payload.instant)
+  reply.send({ code })
+})
+
+fastify.get('/datapoints/:code', async (request, reply) => {
+  const { db } = fastify.mongo
+  const timeSeries = createTimeSeries(db)
+  const res = await timeSeries.fetchDataPoint(request.params.code)
+
+  reply.send(res)
 })
 
 function start (opts, callback) {
--- test/index.test.js
+++ test/index.test.js
@@ -2,19 +2,31 @@ const t = require('tap')
 const test = t.test
 const fastify = require('../index').fastify
 
-test('GET /datapoint', t => {
+test('GET /datapoint/:id returns a single data point', async t => {
   t.plan(2)
 
-  fastify.inject(
-    {
-      method: 'GET',
-      url: '/datapoint'
-    },
-    res => {
-      t.strictEqual(res.statusCode, 200)
-      t.deepEqual(JSON.parse(res.payload), { hello: 'world' })
+  const payload = {
+    data: 'ciao',
+    instant: new Date().getTime()
+  }
 
-      fastify.close()
-    }
-  )
+  const postRes = await fastify.inject({
+    method: 'POST',
+    url: '/datapoints',
+    payload
+  })
+
+  const code = JSON.parse(postRes.payload).code
+
+  const expectePayload = Object.assign({}, payload, { code })
+
+  const res = await fastify.inject({
+    method: 'GET',
+    url: `/datapoints/${code}`
+  })
+
+  t.strictEqual(res.statusCode, 200)
+  t.deepEqual(JSON.parse(res.payload), expectePayload)
+
+  fastify.close()
 })
